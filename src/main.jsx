1d4ba8205f4a929f69b3028c2fbf53678080f9f9
tompat1 <thomasrynell@me.com>
2026-04-01 20:55:37 +0200

    Fix React tooling and ignore backup files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
+import { createElement } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  React.createElement(React.StrictMode, null, React.createElement(App)),
-);
+ReactDOM.createRoot(document.getElementById('root')).render(createElement(App));
